2f14bae741e15fa447825f8d3933e0d61775fe16
maxsos19 <126406905+maxsos19@users.noreply.github.com>
2023-03-14 18:33:55 +0300

    Add files via upload

---
+++ Program3.2.4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_3._2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Myname = "Max";
+
+            Console.WriteLine(Myname);
+            Console.ReadKey();
+        }
+    }
+}
