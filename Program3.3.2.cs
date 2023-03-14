9cf723eab5434375f3e52c994f0edb12aac1a89e
maxsos19 <126406905+maxsos19@users.noreply.github.com>
2023-03-14 20:46:00 +0300

    Add files via upload

---
+++ Program3.3.2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Myname = "Max";
+            byte MyAge = 19;
+            bool HaveIApet = true;
+            double MyShoeSize = 38.5;
+
+            Console.WriteLine("My name is " + Myname);
+            Console.WriteLine("MyAge " + MyAge);
+            Console.WriteLine("Do I have a pet? " + HaveIApet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
+            Console.ReadKey();
+        }
+    }
+}
