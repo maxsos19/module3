dc5e01d6a38d4ee96a98a8a7f9beb4108152ef71
maxsos19 <126406905+maxsos19@users.noreply.github.com>
2023-03-15 23:55:31 +0300

    Add files via upload

---
+++ Program3.6.8.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = 10.0 % 3.0;
+            Console.WriteLine("10.0 % 3.0 = {0} ", result);
+            Console.ReadKey();
+        }
+    }
+}
