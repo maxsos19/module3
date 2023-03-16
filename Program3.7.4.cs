c5939d80d5fe12997ed06755f8a8386962a5682b
maxsos19 <126406905+maxsos19@users.noreply.github.com>
2023-03-16 22:19:33 +0300

    Add files via upload

---
+++ Program3.7.4.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            byte age = checked ((byte)int.Parse(Console.ReadLine()));
+            Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+            Console.ReadKey();
+        }
+    }
+}
